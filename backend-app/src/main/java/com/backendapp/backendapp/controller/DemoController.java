efd5d16c53806ec9fe66fdf1d6777df575d6b06b
harsimran <singh.harsimran1308@gmail.com>
2024-03-25 13:33:45 -0400

    Initial commit

---
+++ backend-app/src/main/java/com/backendapp/backendapp/controller/DemoController.java
@@ -0,0 +1,15 @@
+package com.backendapp.backendapp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/demo")
+public class DemoController {
+
+    @GetMapping("testApi")
+    public String testMethod() {
+        return "I am test string";
+    }
+}
